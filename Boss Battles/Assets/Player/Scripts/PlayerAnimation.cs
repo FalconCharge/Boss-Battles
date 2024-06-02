4248a51e91265d45bc7500219dc01815898fa229
FalconCharge <99128705+FalconCharge@users.noreply.github.com>
2024-06-02 17:06:39 -0300

    Created Player Animation Script
    Move the players animations to another script to keep it simple. this is untested so no clue if it works or not well find out once we have all the animations in order. Player movement should all be working but that went all untested to also deleted the menu since I want to work on a single boss at a time and a menu would be consider polish IMO

---
+++ Boss Battles/Assets/Player/Scripts/PlayerAnimation.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.AnimatedValues;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    private Animator animbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animbody = GetComponent<Animator>();
+
+        if (animbody == null)
+        {
+            Debug.LogError("Animator component not found on the player.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(animbody != null)
+        {
+            HandleAnimations();
+        }
+    }
+    private void HandleAnimations()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        bool isGrounded = GetComponent<PlayerMovement>().GroundCheck();
+
+        animbody.SetFloat("Speed", Mathf.Abs(moveX));
+
+        if(moveY > 0f)
+        {
+            animbody.SetBool("IsJumping", !isGrounded); //In air
+        }
+        else
+        {
+            animbody.SetBool("IsJumping", isGrounded); //On Ground
+        }
+
+
+        if (moveX < 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f); // Facing left
+        }
+        else if (moveX > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f); // Facing right
+        }
+    }
+}
