4c3657252db3df32edabac17713a0a1619499a8a
FalconCharge <99128705+FalconCharge@users.noreply.github.com>
2024-06-08 15:39:52 -0300

    Ignroe

--- Boss Battles/Assets/Player/Scripts/PlayerMovement.cs
+++ Boss Battles/Assets/Player/Scripts/PlayerMovement.cs
@@ -10,9 +10,15 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb { get; private set; }
 
     public bool IsFacingRight { get; private set; }
+    public bool IsJumping { get; private set; }
     public float LastOnGroundTime { get; private set; }
 
     private Vector2 _moveInput;
+    //Jump
+    private bool _isJumpCut;
+    private bool _isJumpFalling;
+
+    public float LastPressedJumpTime { get; private set; }
 
     #endregion
 
@@ -35,23 +41,98 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         IsFacingRight = true;
+        rb.gravityScale = Data.gravityScale;
     }
 
     private void Update()
     {
+        LastOnGroundTime -= Time.deltaTime;
+        LastPressedJumpTime -= Time.deltaTime;
+
         #region InputHandler
         _moveInput.x = Input.GetAxisRaw("Horizontal");
         _moveInput.y = Input.GetAxisRaw("Vertical");
 
         if (_moveInput.x != 0)  
             CheckDirectionToFace(_moveInput.x > 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.J))
+        {
+            OnJumpInput();
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.J))
+        {
+            OnJumpUpInput();
+        }
+
         #endregion
 
         #region collision Checks
-
-        if(Physics2D.Raycast(_groundCheckPoint.position,Vector2.down, 1, _groundLayer))
+        if (!IsJumping)
         {
-            LastOnGroundTime = 0.1f;
+            if (Physics2D.Raycast(_groundCheckPoint.position, Vector2.down, 1, _groundLayer))
+            {
+                LastOnGroundTime = Data.coyoteTime;
+            }
+        }
+        #endregion
+
+        #region JUMP CHECKS
+        if (IsJumping && rb.velocity.y < 0)
+        {
+            IsJumping = false;
+            _isJumpFalling = true;
+        }
+        
+        if (LastOnGroundTime > 0 && !IsJumping)
+        {
+            _isJumpCut = false;
+
+            if (!IsJumping)
+                _isJumpFalling = false;
+        }
+
+        //Jump
+        if (CanJump() && LastPressedJumpTime > 0)
+        {
+            IsJumping = true;
+            _isJumpCut = false;
+            _isJumpFalling = false;
+            Jump();
+        }
+
+        #endregion
+
+        #region GRAVITY
+        //Higher gravity if we've released the jump input or are falling
+        if (rb.velocity.y < 0 && _moveInput.y < 0)
+        {
+            //Much higher gravity if holding down
+            SetGravityScale(Data.gravityScale * Data.fastFallGravityMult);
+            //Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFastFallSpeed));
+        }
+        else if (_isJumpCut)
+        {
+            //Higher gravity if jump button released
+            SetGravityScale(Data.gravityScale * Data.jumpCutGravityMult);
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFallSpeed));
+        }
+        else if ((IsJumping || _isJumpFalling) && Mathf.Abs(rb.velocity.y) < Data.jumpHangTimeThreshold)
+        {
+            SetGravityScale(Data.gravityScale * Data.jumpHangGravityMult);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            //Higher gravity if falling
+            SetGravityScale(Data.gravityScale * Data.fallGravityMult);
+            //Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -Data.maxFallSpeed));
+        }
+        else
+        {
+            //Default gravity if standing on a platform or moving upwards
+            SetGravityScale(Data.gravityScale);
         }
         #endregion
     }
@@ -60,7 +141,20 @@ public class PlayerMovement : MonoBehaviour
     {
         Run();
     }
+    #region INPUT CALLBACKS
+    //Methods which whandle input detected in Update()
+    public void OnJumpInput()
+    {
+        LastPressedJumpTime = Data.jumpInputBufferTime;
+    }
 
+    public void OnJumpUpInput()
+    {
+        if (CanJumpCut())
+            _isJumpCut = true;
+    }
+    #endregion
+    #region Methods
     private void Run()
     {
         float targetSpeed = _moveInput.x * Data.runMaxSpeed;
@@ -94,7 +188,29 @@ public class PlayerMovement : MonoBehaviour
         //Convert this to a vector and apply to rigidbody
         rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
     }
-    #region Methods
+    private void Jump()
+    {
+        //Ensures we can't call Jump multiple times from one press
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+
+        #region Perform Jump
+        //We increase the force applied if we are falling
+        //This means we'll always feel like we jump the same amount 
+        //(setting the player's Y velocity to 0 beforehand will likely work the same, but I find this more elegant :D)
+        float force = Data.jumpForce;
+        if (rb.velocity.y < 0)
+            force -= rb.velocity.y;
+
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        #endregion
+    }
+
+    private void SetGravityScale(float gravityScale)
+    {
+        rb.gravityScale = gravityScale;
+    }
+    
 
     private void Turn()
     {
@@ -111,8 +227,8 @@ public class PlayerMovement : MonoBehaviour
     {
         if (_groundCheckPoint != null)
         {
-            Gizmos.color = Color.red; // You can choose any color you want
-            Gizmos.DrawRay(_groundCheckPoint.position, Vector3.down * 1);
+            //Gizmos.color = Color.red; // You can choose any color you want
+            //Gizmos.DrawRay(_groundCheckPoint.position, Vector3.down * 1);
         }
     }
 
@@ -121,6 +237,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isMovingRight != IsFacingRight)
             Turn();
+    }
+    private bool CanJump()
+    {
+        return LastOnGroundTime > 0 && !IsJumping;
+    }
+    private bool CanJumpCut()
+    {
+        return IsJumping && rb.velocity.y > 0;
     }
     #endregion
 }
